df3efb8bc783576b97cabfabe355482aae13e690
i552011 <91604800+JanBertl@users.noreply.github.com>
2022-05-16 23:02:10 +0200

    Loading data to the MongoDB

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProducts =async (req,res,next) => {
     res.status(201).json(products)
 }
 
-const createSingleProduct =async (req,res,next) => {
+const createSingleProduct = async (req,res,next) => {
     const newProduct = await Product.create(req.body)
     res.status(200).json(newProduct)
 }
---
+++ 04-store-api/starter/populate.js
@@ -0,0 +1,22 @@
+require('express-async-errors')
+require('dotenv').config();
+const mongoose = require('mongoose')
+const connectDB = require('./db/connect')
+const Products = require('./models/product')
+const res = require('express/lib/response')
+
+const data = require('./products.json')
+
+const loadData = async ()=>{
+    await connectDB(process.env.MONGO_URI)
+    await Products.deleteMany();
+    await Products.create(data);
+}
+
+loadData();
+
+require('dotenv').config()
+// absolute Kurzform
+    // await mongoose.connect(process.env.MONGO_URI)
+    // await mongoose.model('Products').deleteMany()
+    // await mongoose.model('Products').create(data)
